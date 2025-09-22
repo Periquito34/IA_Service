4e190547995b79b355ed5b119c1f980d9873e088
Miguel Angel Rodriguez C. <miguelrc055@gmail.com>
2025-09-22 18:00:50 -0500

    cambios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import app from './app.js';
-import { config } from './config/env.js';
+import app from './src/app.js';
+import { config } from './src/config/env.js';
 
 app.listen(config.port, () => {
   console.log(`AI Service listening on :${config.port}`);
